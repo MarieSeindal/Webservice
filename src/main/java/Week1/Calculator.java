88bd567d94b69a2883324f07886093b49ad81cbd
Marie <S185363@student.dtu.dk>
2022-01-03 14:00:16 +0100

    test

--- src/main/java/Week1/Calculator.java
+++ src/main/java/Week1/Calculator.java
@@ -2,8 +2,23 @@ package Week1;
 
 public class Calculator {
 
-    public int add(int a, int b){
-        return a+b;
+    public int add(String numbers){
+        int value =0;
+
+        switch (numbers.length()){
+            case 0:
+                break;
+            case 1:
+                value = Integer.parseInt(numbers);
+                return value;
+            case 3:
+                numbers.split(",");
+
+
+        }
+
+
+        return 0;
     }
 
 }
--- src/test/java/Week1/CalculatorTest.java
+++ src/test/java/Week1/CalculatorTest.java
@@ -7,13 +7,29 @@ import static org.junit.jupiter.api.Assertions.*;
 class CalculatorTest {
 
     @Test
-    void add() {
+    void add2() {
         int expected = 3;
         Calculator calc = new Calculator();
-        int actual = calc.add(1,2);
+        int actual = calc.add("1,2");
 
+        assertEquals(expected, actual, "2 numbers");
+    }
+    @Test
+    void add1() {
+        int expected = 1;
+        Calculator calc = new Calculator();
+        int actual = calc.add("1");
 
-        assertEquals(expected, actual);
+        assertEquals(expected, actual, "1 number");
+    }
+
+    @Test
+    void add0() {
+        int expected = 0;
+        Calculator calc = new Calculator();
+        int actual = calc.add("");
+
+        assertEquals(expected, actual, "1 number");
     }
 
 }
