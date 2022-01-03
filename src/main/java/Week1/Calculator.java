07e6b4a2c1d9ffadcb71c88fb3991fa099706e48
Marie <S185363@student.dtu.dk>
2022-01-03 11:44:16 +0100

    Initial commit

---
+++ src/main/java/Week1/Calculator.java
@@ -0,0 +1,9 @@
+package Week1;
+
+public class Calculator {
+
+    public int add(int a, int b){
+        return a+b;
+    }
+
+}
---
+++ src/main/java/Week1/main.java
@@ -0,0 +1,13 @@
+package Week1;
+
+public class main {
+    public static void main(String[] args) {
+        System.out.println("Hej");
+
+    }
+
+    public
+
+
+
+}
