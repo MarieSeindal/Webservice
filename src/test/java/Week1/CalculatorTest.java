c6a46588bd527cf3b1f06ce5d078468475c554c6
Marie <S185363@student.dtu.dk>
2022-01-03 11:53:54 +0100

    First

---
+++ src/test/java/Week1/CalculatorTest.java
@@ -0,0 +1,14 @@
+package Week1;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class CalculatorTest {
+
+    @Test
+    void add() {
+        System.out.println("Test ran");
+        fail("Not implemented yet");
+    }
+}
